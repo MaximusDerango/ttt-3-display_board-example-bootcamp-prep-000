d1063e6b69546c13cee8cd9585d685388243f1ba
Maxim Dalton <maximcdalton@gmail.com>
2018-06-01 18:51:12 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,9 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-  vert = ([" "*3]*3).join("|")
-  hor = "\n" + "-"*11 + "\n"
+  rows = 3
+  cols = 3
+  cell_size = 3
+  vert = ([" "*cell_size]*cols).join("|")
+  hor = "\n" + "-"*() + "\n"
   puts ([vert]*3).join(hor)
 end
